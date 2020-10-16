4a9ea996ffb75ff2358eb21a438c32047a794ed0
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 13:38:06 +0700

    Program Biodata

---
+++ src/pbo2/pkg10116374/latihan1/biodata/PBO210116374Latihan1Biodata.java
@@ -0,0 +1,31 @@
+package pbo2.pkg10116374.latihan1.biodata;
+/*
+ * @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk menampilkan biodata diri
+ */
+public class PBO210116374Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("-------------------------------------------------");
+        System.out.println("BIODATA" + "\n" + "-------------------------------------------------");
+        System.out.println("NAMA\t\t:   Teuku Rizki Nabil");
+        System.out.println("ALAMAT\t\t:   Jl. Titimplik Dalam No 151");
+        System.out.println("TEMPAT LAHIR\t:   Purwakarta");
+        System.out.println("TANGGAL LAHIR\t:   09 September 1998");
+        System.out.println("-------------------------------------------------");
+        System.out.println("HOBI\t\t:   Menggambar");
+        System.out.println("MAKANAN FAVORIT\t:   Labu");
+        System.out.println("TANGGAL FAVORIT\t:   09 September 1998");
+        System.out.println("-------------------------------------------------");
+        
+        
+    }
+    
+}
